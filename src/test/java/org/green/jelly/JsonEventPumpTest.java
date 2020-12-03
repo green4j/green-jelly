e9788773dab56120333bc9d77c4eecc0fdd09b6e
gudkov <gudkovanatoly@hotmail.com>
2020-12-03 14:28:05 +0300

    Code style fixed

--- src/test/java/org/green/jelly/JsonEventPumpTest.java
+++ src/test/java/org/green/jelly/JsonEventPumpTest.java
@@ -5,12 +5,12 @@ import org.junit.Test;
 
 public class JsonEventPumpTest {
 
-    private static final String testJson = "{" +
-            "\"a\": 1," +
-            "\"b\": null," +
-            "\"c\": [true, false, null, 1, \"string\", " +
-            "{ \"cc_1\": [], \"cc_2\": [1, 2, 4], \"cc_3\": \"cc_3 value\"}]" +
-            "}";
+    private static final String TEST_JSON = "{"
+        + "\"a\": 1,"
+        + "\"b\": null,"
+        + "\"c\": [true, false, null, 1, \"string\", "
+        + "{ \"cc_1\": [], \"cc_2\": [1, 2, 4], \"cc_3\": \"cc_3 value\"}]"
+        + "}";
 
     @Test
     public void test() {
@@ -19,7 +19,7 @@ public class JsonEventPumpTest {
         final JsonEvents expectedEvents = new JsonEvents();
         parser.setListener(expectedEvents);
 
-        parser.parseAndEoj(testJson);
+        parser.parseAndEoj(TEST_JSON);
 
         final StringBuilder resultJson = new StringBuilder();
 
@@ -27,7 +27,7 @@ public class JsonEventPumpTest {
         final JsonEventPump pump = new JsonEventPump(generator);
         parser.setListener(pump);
 
-        parser.parseAndEoj(testJson);
+        parser.parseAndEoj(TEST_JSON);
 
         final JsonEvents resultEvents = new JsonEvents();
         parser.setListener(resultEvents);
