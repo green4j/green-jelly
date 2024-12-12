3bbcc849acf121ca20f6506bdace5cdbc1cc4c5f
Anatolii Gudkov <agudkov@deltixlab.com>
2024-12-12 08:45:48 +0100

    Codestyle fixed

--- src/test/java/io/github/green4j/jelly/Utf8ByteArrayWriterTest.java
+++ src/test/java/io/github/green4j/jelly/Utf8ByteArrayWriterTest.java
@@ -30,7 +30,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class Utf8ByteArrayWriterTest {
 
